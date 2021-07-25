3cbdddf33a124e1a5d5795a528ac5b63222cd84a
Alfred Sorrow <kurtsorrow@gmail.com>
2021-07-25 23:34:31 +0300

    updating example code

--- public/index.php
+++ public/index.php
@@ -3,7 +3,11 @@
 use Slim\Psr7\Response;
 use Slim\Psr7\Factory\ServerRequestFactory;
 use Slim\Psr7\Factory\StreamFactory;
+use Framework\Http\Router\Router;
 use Framework\Http\ResponseSender;
+use Framework\Http\Router\Result;
+use Framework\Http\Router\RouteCollection;
+use Psr\Http\Message\ServerRequestInterface;
 
 chdir(dirname(__DIR__));
 ini_set('display_errors', '1');
@@ -15,17 +19,15 @@ $request = ServerRequestFactory::createFromGlobals();
 
 $patch = $request->getUri()->getPath();
 
-### preprocessing
+$routes = new RouteCollection();
+$routes->get('home', '/', fn () => (new Response())->withBody((new StreamFactory())->createStream("Hello, Guest!")));
+$router = new Router($routes);
 
-
-### action
-$name = $request->getQueryParams()['name'] ?? 'Guest';
-$body = (new StreamFactory())->createStream("Hello, {$patch}!");
-$response = (new Response())->withBody($body);
+$result = $router->match($request);
+$response = $result->getHandler()($request, $result->getName(), $result->getAttributes());
 
 ### postprocessing
 $response = $response->withHeader('ASS', 'SAS');
 
 ### send
-
 ResponseSender::send($response);
